7abf8e96cec9aab64d9f33c538caab7e7e017159
VolodyaZ5 <volodyaz5@mail.ru>
2019-04-04 14:57:42 +0300

    Сдача домашнего задания: урок-4

---
+++ AppealToOrderBy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** Дан фрагмент программы:
+ 
+Dictionary < string , int > dict = new Dictionary < string , int >()
+{
+    { "four" , 4 },
+    { "two" , 2 },
+    { "one" , 1 },
+    { "three" , 3 },
+};
+var d = dict . OrderBy ( delegate ( KeyValuePair < string , int > pair ) {
+return pair . Value ; });
+foreach ( var pair in d)
+{
+    Console . WriteLine ( "{0} - {1}" , pair . Key , pair . Value );
+}
+
+    а. Свернуть обращение к OrderBy с использованием лямбда-выражения =>.
+    b. * Развернуть обращение к OrderBy с использованием делегата .
+     */
+
+namespace AppealToOrderBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"four", 4 },
+                {"two", 2 },
+                {"one", 1 },
+                {"three", 3 }
+            };
+            var d = dict.OrderBy(pair => pair.Value);
+            foreach (var pair in d)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CountOfElementInList/MyList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfElementInList
+{
+    /// <summary>
+    /// Обобщенный класс
+    /// </summary>
+    /// <typeparam name="T">Тип класса</typeparam>
+    class MyList<T>
+    {        
+        T[] list = new T[3];
+        private int index;
+
+        /// <summary>
+        /// Метод вычисляет количество одинаковых элементов
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<T, int> CountOfSameObjects()
+        {
+            Dictionary<T, int> res = new Dictionary<T, int>();
+            for (int i = 0; i < index; i++)
+            {
+                if (res.ContainsKey(list[i]))
+                {
+                    res[list[i]]++;
+                }
+                else
+                {
+                    res.Add(list[i], 1);
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Добавление элемента
+        /// </summary>
+        /// <param name="item">Добавляемый элемент</param>
+        public void Add(T item)
+        {
+            if (index >= list.Length)
+            {
+                Array.Resize(ref list, list.Length * 2);
+            }
+            list[index++] = item;
+        }
+
+        /// <summary>
+        /// Удаление элемента
+        /// </summary>
+        /// <param name="Index">Удаляемый элемент</param>
+        public void RemoveAt(int Index)
+        {
+            for (int i = Index; i < index; i++)
+            {
+                list[i] = list[i + 1];
+            }
+            index--;
+        }
+
+    }
+}
